ce5b3f0f3736234746a6abddf741961df8d84c5f
Dominique <dbelhachemi@454.bio>
2023-06-11 23:24:04 -0400

    add database

--- bio/bio/settings.py
+++ bio/bio/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'bio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'biodb',
+        'USER': 'djangouser',
+        'PASSWORD': 'todofixme',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
