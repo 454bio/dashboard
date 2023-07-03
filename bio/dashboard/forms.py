68c8e98ed6e8c00b2913acf6497ec8df3ecb30a7
Dominique <dbelhachemi@454.bio>
2023-07-03 14:44:40 -0400

    add label for roiset.zip upload

--- bio/dashboard/forms.py
+++ bio/dashboard/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class RoiSetForm(forms.Form):
     title = forms.CharField(label='Label', max_length=50)
-    file = forms.FileField()
+    file = forms.FileField(label='RoiSet File')
