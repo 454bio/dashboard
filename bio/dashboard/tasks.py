ced980328b3bbe6555a0bcb6f89246f241ceb10f
Dominique <dbelhachemi@454.bio>
2023-07-03 10:39:39 -0400

    add basic celery tasks.py

--- bio/dashboard/tasks.py
+++ bio/dashboard/tasks.py
@@ -1,13 +1,40 @@
 from celery import shared_task
+import ziontools
+import os
+import subprocess
+import shlex
 
 @shared_task
-def run_pixel_extraction(somevar):
+def run_pixel_extraction(raw_path, report_path, roiset_zip_filename):
     print("run_pixel_extraction")
-    return True
+
+    full_roiset_path = os.path.join(report_path, roiset_zip_filename)
+    full_spot_pixel_data_path = os.path.join(report_path, "spot_pixel_data.csv")
+
+    ziontools.extract_roiset_pixel_data(
+        raw_path,
+        full_roiset_path,
+        full_spot_pixel_data_path,
+        200,  # max_number_of_pixel_per_spot=200,
+        0     # start_645_image_number=0 auto
+    )
+
+    df = ziontools.calculate_and_apply_transformation(
+        full_spot_pixel_data_path,
+        full_roiset_path,
+        raw_path,
+        report_path,
+        {},
+        {}
+    )
+
+    return 9
+
 
 @shared_task
 def mul(x, y):
     return x * y
+
 
 @shared_task
 def xsum(numbers):
